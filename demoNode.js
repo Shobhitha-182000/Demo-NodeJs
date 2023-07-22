37e4a33ada16205b45fa089a43f32a89429b9e07
Shobhitha-182000 <shobhithaas18@gmail.com>
2023-07-22 09:46:23 +0530

    redirectt

--- demoNode.js
+++ demoNode.js
@@ -5,17 +5,27 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url === "/") {
     res.write(
-      '<html><body><h1> Welcome to my Node Js project</h1><form action="/home" method="POST"><button type="Submit">go to home</button></form></body></html>'
+      '<html><body><h1> Welcome to my Node Js project</h1><form action="/home" method="POST"><input type="text" name="message"><button type="Submit">go to home</button></form></body></html>'
     );
     return res.end();
   }
-  if (url === "/home") {
-    res.write(
-      '<html><body><h1> Welcome  Home</h1><form action="/about" method="POST"><button type="Submit">go to about</button></form></body></html>'
-    );
-    return res.end();
+  if (url === "/home" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+    const message=parseBody.split('=')[1];
+      fs.writeFileSync("text.txt", message);
+      res.write(
+        `<html><body><h1> Welcome  Home</h1><form action="/" method="POST"><h3>entered name is ${message}</h3><button type="Submit">go to about</button></form></body></html>`
+      );
+      return res.end();
+    });
   }
-  if (url === "/about") {
+  if (url === "/about" && method === "POST") {
     res.write(
       '<html><body><h1> Welcome to about Us page</h1><form action="/" method="POST"><button type="Submit">go to first page</button></form></body></html>'
     );
